847e671ed36931570e0a847e5b8cbb90556a0dfc
pedrorosarioo <pedro.ufba2012@hotmail.com>
2017-05-10 01:41:40 -0300

    Update criandoarquivo.asm

--- criandoarquivo.asm
+++ criandoarquivo.asm
@@ -4,27 +4,27 @@ org 100h
 filename db "E:\testando.txt", 0
 handle dw ?
 erro db "Erro de criacao de arquivo $"
-string db 255 DUP(13)
+;string db 255 DUP(13)
 
 .code
     mov ax, @data
     mov ds, ax
     main proc
-        mov ah, 3ch
+        mov ah, 3ch ;CRIAR ARQUIVO
         mov dx, offset filename
         int 21h
         mov handle, ax
-        jc fail
+        jc fail ;CASO CF=1 ENCERRE O PROGRAMA
         jmp fimprog
         fail:
-            mov dx, offset erro
-            mov ah, 09h
+            mov dx, offset erro 
+            mov ah, 09h ; PRINTA MENSAGEM DE ERRO
             int 21h
         endp
         fimprog:
-            mov ah, 3eh
+            mov ah, 3eh ;FECHA ARQUIVO
             int 21h
-            mov ax, 4ch
+            mov ax, 4ch ;ENCERRA O PROGRAMA
             int 21h
         endp
      endp
