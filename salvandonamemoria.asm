a34f706deb6bb37f674c136878812837d37c47ec
pedrorosarioo <pedro.ufba2012@hotmail.com>
2017-05-10 21:39:14 -0300

    Update salvandonamemoria.asm

--- salvandonamemoria.asm
+++ salvandonamemoria.asm
@@ -9,13 +9,13 @@ string db 255 DUP(13)
     main proc
         mov dh, 0
         mov dl, 'a'
-        push dx
+        push dx ;mova 'a' para o topo da pilha
         mov dl, 'b'
-        push dx
+        push dx ;mova 'b' para o topo da pilha
         mov dl, 'c'
-        pop dx
-        mov ah, 02h
-        int 21h
+        pop dx ; passe o topo da pilha para o registrador dx ('b')
+        mov ah, 02h ; exibir caractere na tela
+        int 21h ; exibe o caractere 'b'
         mov ah, 4ch
         int 21h
         
