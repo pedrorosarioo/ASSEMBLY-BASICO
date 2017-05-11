9e8bd41156c270a8b995b5119cc298fd37d65c8d
pedrorosarioo <pedro.ufba2012@hotmail.com>
2017-05-10 22:08:15 -0300

    Update criandoarquivo.asm

--- criandoarquivo.asm
+++ criandoarquivo.asm
@@ -2,20 +2,19 @@
 org 100h
 .data
 filename db "E:\testando.txt", 0
-handle dw ?
+handle dw ? ;variavel criada para armazenar o handle, uma vez que o registrador AX será sobrescrito
 erro db "Erro de criacao de arquivo $"
-;string db 255 DUP(13)
 
 .code
     mov ax, @data
     mov ds, ax
     main proc
-        mov ah, 3ch ;CRIAR ARQUIVO
-        mov dx, offset filename
-        int 21h
-        mov handle, ax
-        jc fail ;CASO CF=1 ENCERRE O PROGRAMA
-        jmp fimprog
+        mov ah, 3ch ; INSTRUÇÃO CRIAR ARQUIVO
+        mov dx, offset filename ;passa para dx o nome/endereço do arquivo a ser criado
+        int 21h ; CRIA ARQUIVO, AX PASSA A ARMAZENAR O HANDLE DO ARQUIVO
+        mov handle, ax ;guarde o filehandle na variavel 'handle'
+        jc fail ;CASO CF=1 PULE PARA A ROTINA FAIL
+        jmp fimprog; SE CF=0 PULE PARA A ROTINA FIMPROG
         fail:
             mov dx, offset erro 
             mov ah, 09h ; PRINTA MENSAGEM DE ERRO
